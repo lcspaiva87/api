c2de5b20aa9509af40809432ab9448f6c74be01f
Lucas Paiva <lcspaiva87@gmail.com>
2024-11-16 13:13:12 -0300

    Adiciona implementação da API de classes com modelo, repositório e controlador

--- src/main/java/com/School_Registration/api/Class/ClassController.java
+++ src/main/java/com/School_Registration/api/Class/ClassController.java
@@ -7,25 +7,26 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.School_Registration.api.Class.ClassModel;
+
 @RestController
 @RequestMapping("/classes")
 public class ClassController {
-    @Autowired
-    private IClassRepository classRepository;
+  @Autowired
+  private IClassRepository classRepository;
 
-    @PostMapping("/")
-    public ResponseEntity create(@RequestBody ClassModel classModel) {
-        var existingClass = this.classRepository.findByNameAndYear(
-            classModel.getName(), 
-            classModel.getYear()
-        );
+  @PostMapping("/")
+  public ResponseEntity create(@RequestBody ClassModel classModel) {
+    var existingClass = this.classRepository.findByNameAndYear(
+        classModel.getName(),
+        classModel.getYear());
 
-        if(existingClass != null) {
-            return ResponseEntity.status(400)
-                .body("Class already exists for this year");
-        }
-
-        var classCreated = this.classRepository.save(classModel);
-        return ResponseEntity.status(201).body(classCreated);
+    if (existingClass != null) {
+      return ResponseEntity.status(400)
+          .body("Class already exists for this year");
     }
-} 
+
+    var classCreated = this.classRepository.save(classModel);
+    return ResponseEntity.status(201).body(classCreated);
+  }
+}
--- src/main/java/com/School_Registration/api/Class/IClassRepository.java
+++ src/main/java/com/School_Registration/api/Class/IClassRepository.java
@@ -1,5 +1,6 @@
 package com.School_Registration.api.Class;
 
+import org.jboss.logging.processor.generator.model.ClassModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface IClassRepository extends JpaRepository<ClassModel, Long> {
